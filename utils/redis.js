f6550d10d9ab357211a78da6aa66b806655a8df9
frankkib <frankkibagend13@gmail.com>
2023-10-17 07:33:05 +0000

    fixed the catch error

---
+++ utils/redis.js
@@ -0,0 +1,56 @@
+const redis = require('redis');
+
+class RedisClient {
+	constructor() {
+		this.client = redis.createClient();
+		this.client.on('error', (error) => {
+			console.error('Redis Error', error);
+		});
+	}
+	isAlive () {
+		return this.client.connected;
+	}
+	async get(key) {
+		try {
+			const value = await new Promise((resolve, reject) => {
+				this.client.get(key, (error, value) => {
+					if (error) {
+						reject(error);
+					} else {
+						resolve(value);
+					}
+				});
+			});
+			return value;
+		}catch (error) {
+			console.error('Error getting value:', error);
+			throw error;
+		}
+	}
+	async set(key, value, durationInSeconds) {
+		return new Promise((resolve, reject) => {
+			this.client.setex(key, durationInSeconds, value, (error) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve('OK');
+				}
+			});
+		});
+	}
+	async del(key) {
+		return new Promise((resolve, reject) => {
+			this.client.del(key, (error, count) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(count);
+				}
+			});
+		});
+	}
+}
+
+const redisClient = new RedisClient();
+
+module.exports = { redisClient };
