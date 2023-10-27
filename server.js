cfbe95c28db639d61c30074073b297d2915ba109
frankkib <frankkibagend13@gmail.com>
2023-10-27 12:15:37 +0000

    fixed the eslint error

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 5000;
 const routes = require('./routes/index');
@@ -6,6 +7,5 @@ const routes = require('./routes/index');
 app.use('/api', routes);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-
