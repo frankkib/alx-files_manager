5ecac2d3199673b462e67f8f2b59e04634ccb304
frankkib <frankkibagend13@gmail.com>
2023-11-01 10:27:42 +0000

    fixed the routes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const routes = require('./routes/index');
 
-app.use('/api', routes);
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
