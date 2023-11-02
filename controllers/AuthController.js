68b8820002264c5e133aba1919632ba10d0e36d0
frankkib <frankkibagend13@gmail.com>
2023-11-02 08:48:29 +0000

    setup basic authentication

---
+++ controllers/AuthController.js
@@ -0,0 +1,57 @@
+const crypto = require('crypto');
+const uuid = require('uuid');
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AuthController {
+  static async getConnect(req, res) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+    const [email, password] = credentials.split(':');
+
+    if (!email || !password) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const db = dbClient.client.db(process.env.DB_DATABASE);
+    const usersCollection = db.collection('users');
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    const user = await usersCollection.findOne({ email, password: hashedPassword });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const token = uuid.v4();
+    const key = `auth_${token}`;
+
+    redisClient.set(key, user._id.toString(), 86400);
+
+    return res.status(200).json({ token });
+  }
+
+  static async getDisconnect(req, res) {
+    const token = req.header('X-Token');
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    redisClient.del(key);
+    return res.status(204).send();
+  }
+}
+
+module.exports = AuthController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 class UsersController {
@@ -29,6 +30,30 @@ class UsersController {
 
     // Return the response to indicate function completion
     return res;
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const db = dbClient.client.db(process.env.DB_DATABASE);
+    const usersCollection = db.collection('users');
+    const user = await usersCollection.findOne({ _id: userId });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ email: user.email, id: user._id });
   }
 }
 
