b0346bf3549a4c86bbb6f63a54da781ede3e2c85
frankkib <frankkibagend13@gmail.com>
2024-09-18 10:03:51 +0000

    Changed from commn js module to ES modules

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,15 +1,15 @@
-const { redisClient } = require('../utils/redis');
-const { dbClient } = require('../utils/db');
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class AppController {
   static async getStatus(req, res) {
-    const redisStatus = await redisClient.isAlive();
-    const dbStatus = await dbClient.isAlive();
+    const isRedisAlive = await redisClient.isAlive();
+    const isDBAlive = await dbClient.isAlive();
 
-    if (redisStatus && dbStatus) {
+    if (isRedisAlive && isDBAlive) {
       res.status(200).json({ redis: true, db: true });
     } else {
-      res.status(500).json({ redis: redisStatus, db: dbStatus });
+      res.status(500).json({ redis: isRedisAlive, db: isDBAlive });
     }
   }
 
@@ -17,12 +17,12 @@ class AppController {
     const usersCount = await dbClient.nbUsers();
     const filesCount = await dbClient.nbFiles();
 
-    if (usersCount >= 0 && filesCount >= 0) {
+    if (usersCount !== -1 && filesCount !== -1) {
       res.status(200).json({ users: usersCount, files: filesCount });
     } else {
-      res.status(500).json({ error: 'Error retrieving stats' });
+      res.status(500).json({ users: 'Error', files: 'Error' });
     }
   }
 }
 
-module.exports = AppController;
+export default AppController;
