a42d5cfa39abbc877242a917e3fbc88c40d29f03
frankkib <frankkibagendi13@gmail.com>
2024-09-20 10:03:17 +0300

    fix the eslint error

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
 import UserController from '../controllers/UsersController';
-import FilesController from '../controllers/FilesController';
-import validateToken from '../middleware/valtokens';
+// import FilesController from '../controllers/FilesController';
+// import validateToken from '../middleware/valtokens';
 
 const router = Router();
 
@@ -13,6 +13,6 @@ router.post('/users', UserController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UserController.getMe);
-router.post('/files', validateToken, FilesController.postUpload);
+// vrouter.post('/files', validateToken, FilesController.postUpload);
 
 export default router;
