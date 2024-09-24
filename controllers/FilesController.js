9d3656d28326e3920df58379f7ac70bf74f7b7cf
frankkib <frankkibagendi13@gmail.com>
2024-09-24 09:55:34 +0300

    added anew API endpoint for get data by id

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -43,10 +43,10 @@ class FilesController {
           _id: ObjectId(parentId),
         });
         if (!parentFile) {
-          return res.status(400).json({ error: 'Parent not found' });
+          return res.status(400).json({ error: 'Invalid parentId' });
         }
         if (parentFile.type !== 'folder') {
-          return res.status(400).json({ error: 'Parent is not a folder' });
+          return res.status(400).json({ error: 'ParentId not a folder' });
         }
       }
 
